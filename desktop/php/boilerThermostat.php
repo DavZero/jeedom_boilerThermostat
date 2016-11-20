6b5653a61ee359d9b54f6b6a303fed7044330f9d
DavZero <david.vinet@gmail.com>
2016-11-20 14:14:16 +0100

    Improve UI

--- desktop/php/boilerThermostat.php
+++ desktop/php/boilerThermostat.php
@@ -37,7 +37,7 @@ $eqLogicsManager = eqLogic::byTypeAndSearhConfiguration('boilerThermostat','Mana
   </div>
 
   <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend><i class="fa fa-table"></i>{{Mes equipements}}</legend>
+    <legend><i class="fa fa-table"></i> {{Mes equipements}} </legend>
 
     <div class="eqLogicThumbnailContainer">
       <?php
@@ -89,7 +89,7 @@ $eqLogicsManager = eqLogic::byTypeAndSearhConfiguration('boilerThermostat','Mana
     <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
     <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
     <a class="btn btn-default eqLogicAction pull-right" data-action="configure"><i class="fa fa-cogs"></i> {{Configuration avancée}}</a>
-    <a class="btn btn-xs btn-default pull-right eqLogicAction thermostat" data-action="copy" hidden><i class="fa fa-files-o"></i>{{Dupliquer}}</a>
+    <a class="btn btn-default eqLogicAction pull-right thermostat" data-action="copy" hidden><i class="fa fa-files-o"></i> {{Dupliquer}}</a>
 
     <ul class="nav nav-tabs" role="tablist">
       <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
@@ -99,101 +99,119 @@ $eqLogicsManager = eqLogic::byTypeAndSearhConfiguration('boilerThermostat','Mana
 
     <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
       <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-        <form class="form-horizontal">
-          <fieldset>
-            <br/>
-            <div class="form-group">
-              <label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
-              <div class="col-sm-3">
-                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement boilerThermostat}}"/>
+        <div class="row">
+          <div class="col-sm-6">
+            <form class="form-horizontal">
+              <fieldset>
+                <br/>
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">{{Nom de l'équipement}}</label>
+                  <div class="col-sm-8">
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement boilerThermostat}}"/>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                  <div class="col-sm-8">
+                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                      <option value="">{{Aucun}}</option>
+                      <?php
+                      foreach (object::all() as $object) {
+                        echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                      }
+                      ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">{{Catégorie}}</label>
+                  <div class="col-sm-8">
+                    <?php
+                    foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                      echo '<label class="checkbox-inline">';
+                      echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                      echo '</label>';
+                    }
+                    ?>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-3 control-label" >{{Activer}}</label>
+                  <div class="col-sm-8">
+                    <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                    <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+                  </div>
+                  <!--<div class="col-sm-9">
+                  <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
+                  <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
+                </div>-->
               </div>
-            </div>
-            <div class="form-group">
-              <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-              <div class="col-sm-3">
-                <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                  <option value="">{{Aucun}}</option>
-                  <?php
-                  foreach (object::all() as $object) {
-                    echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                  }
-                  ?>
-                </select>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="col-sm-2 control-label">{{Catégorie}}</label>
-              <div class="col-sm-8">
-                <?php
-                foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
-                  echo '<label class="checkbox-inline">';
-                  echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
-                  echo '</label>';
-                }
-                ?>
-              </div>
-            </div>
-            <div class="form-group">
-              <label class="col-sm-3 control-label" >{{Activer}}</label>
-              <div class="col-sm-8">
-                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-              </div>
-              <!--<div class="col-sm-9">
-                <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Activer}}" data-l1key="isEnable" checked/>
-                <input type="checkbox" class="eqLogicAttr bootstrapSwitch" data-label-text="{{Visible}}" data-l1key="isVisible" checked/>
-              </div>-->
-            </div>
-            <div class="manager" hidden><br/></div>
-            <div class="thermostat" hidden>
-              <legend>{{Configuration}}</legend>
-              <div class="form-group">
-                <label class="col-sm-3 control-label">{{Sonde de température}}</label>
-                <div class="col-sm-3">
-                  <div class="input-group">
-                    <input id="temperatureSensor" type='text' class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="temperatureSensor"/>
-                    <span class="input-group-btn">
-                      <a class="btn btn-default listCmdNumericInfo"><i class="fa fa-list-alt"></i></a>
-                    </span>
+            </fieldset>
+          </form>
+        </div>
+        <div class="col-sm-6">
+          <form class="form-horizontal">
+            <fieldset>
+              <br/>
+              <div class="manager" hidden><br/></div>
+              <div class="thermostat" hidden>
+                <!--<legend>{{Configuration}}</legend>-->
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">{{Sonde de température}}</label>
+                  <div class="col-sm-8">
+                    <div class="input-group">
+                      <input id="temperatureSensor" type='text' class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="temperatureSensor"/>
+                      <span class="input-group-btn">
+                        <a class="btn btn-default listCmdNumericInfo"><i class="fa fa-list-alt"></i></a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">{{Hystérésis}}</label>
+                  <div class="col-sm-6">
+                    <input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="hysteresis" step="0.25" min="0" max="2" value="0.25"/>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-3 control-label">{{Coéfficent d'inertie (valeur entre 0 et 90)}}</label>
+                  <div class="col-sm-6">
+                    <input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="inertiaFactor" step="1" min="0" max="90" value="0"/>
                   </div>
                 </div>
               </div>
-              <div class="form-group">
-                <label class="col-sm-3 control-label">{{Hystérésis}}</label>
-                <div class="col-lg-1">
-                  <input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="hysteresis" step="0.25" min="0" max="2" value="0.25"/>
-                </div>
-              </div>
-              <div class="form-group">
-                <label class="col-sm-3 control-label">{{Coéfficent d'inertie (valeur entre 0 et 90)}}</label>
-                <div class="col-lg-1">
-                  <input type="number" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="inertiaFactor" step="1" min="0" max="90" value="0"/>
-                </div>
-              </div>
-              <legend>{{Modes}} <a class="btn btn-success btn-xs pull-right" id="bt_addMode" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter mode}}</a></legend>
-              <div id="div_modes"></div>
-              <legend>{{Actionneurs (Propagation des evenements On/Off et changement de consigne)}} <a class="btn btn-success btn-xs pull-right" id="bt_addActuator" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter actionneur}}</a></legend>
-              <div id="div_actuators"></div>
-            </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+      <div class="thermostat" hidden>
+        <form class="form-horizontal">
+          <fieldset>
+            <legend>{{Modes}} <a class="btn btn-success btn-xs pull-right" id="bt_addMode" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter mode}}</a></legend>
+            <div id="div_modes"></div>
+            <legend>{{Actionneurs (Propagation des evenements On/Off et changement de consigne)}} <a class="btn btn-success btn-xs pull-right" id="bt_addActuator" style="margin-top: 5px;"><i class="fa fa-plus-circle"></i> {{Ajouter actionneur}}</a></legend>
+            <div id="div_actuators"></div>
           </fieldset>
         </form>
       </div>
-
-      <div role="tabpanel" class="tab-pane" id="commandtab">
-        <table id="table_cmd" class="table table-bordered table-condensed">
-          <thead>
-            <tr>
-              <th>{{Nom}}</th><th>{{Paramètre}}</th><th>{{Option}}</th><th>{{Action}}</th>
-            </tr>
-          </thead>
-          <tbody>
-          </tbody>
-        </table>
-      </div>
-
     </div>
-  </div>
 
-  <?php include_file('desktop', 'boilerThermostat', 'js', 'boilerThermostat');?>
-  <?php include_file('core', 'plugin.template', 'js');?>
+    <div role="tabpanel" class="tab-pane" id="commandtab">
+      <br/>
+      <table id="table_cmd" class="table table-bordered table-condensed">
+        <thead>
+          <tr>
+            <th>{{Nom}}</th><th>{{Paramètre}}</th><th>{{Option}}</th><th>{{Action}}</th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+
+  </div>
+</div>
+
+<?php include_file('desktop', 'boilerThermostat', 'js', 'boilerThermostat');?>
+<?php include_file('core', 'plugin.template', 'js');?>
